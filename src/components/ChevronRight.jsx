91e8d60b17eeb1987b3019807d25570764e09c25
Mohammad Osama <94366139+Znayushchiye@users.noreply.github.com>
2023-12-10 03:29:38 +0530

    Delete src/components directory

--- src/components/ChevronRight.jsx
+++
@@ -1,9 +0,0 @@
-import chevronRight from "./chevronRight.svg";
-
-export const ChevronRight = ({
-  override,
-}) => {
-  return <img className="w-6 h-6" src={chevronRight.src} style={override} />;
-};
-
-export default ChevronRight;
--- src/components/Edit_02.jsx
+++
@@ -1,9 +0,0 @@
-import edit_02 from "./edit_02.svg";
-
-export const Edit_02 = ({ override }) => {
-  return (
-    <img className="w-[23px] h-[23px]" src={edit_02.src} style={override} />
-  );
-};
-
-export default Edit_02;
